6d10ad4dc944e30ab6b3f89daf29cf82c9ecba45
Artem <artem_1285@mail.ru>
2022-11-28 14:51:47 +0500

    lec2

---
+++ lec.py
@@ -0,0 +1,28 @@
+# print ('Введите а');
+#a = float  (input())
+#print ('Введите b');
+#b = float (input ())
+#print(a, ' + ', b, ' = ', a+b)
+#f  = [1,2,3,4]
+#print (f)
+#print (not 2 in f)
+
+#s_odd = not f [0] % 2 # то есть мы обращаемся к цифре 1 и находим остаток от деления на 2
+#print (is_odd)
+# def f(x):
+#     if x == 1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#      return
+# а = (3,4,5)
+# # print (а)
+# # print (а[0])
+
+# for item in a:
+#     print (item)
+# dictionary  = {}
+# for к in dictionary.values ():
+colors = {'red', 'green', 'blue'}
+print(type(colors)) 
